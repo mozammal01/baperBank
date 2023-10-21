37bca93aff8e304a10e1d19a34f0822c14854dc4
Mozammal Haq <mozammalhaq01@gmail.com>
2023-10-21 10:38:49 +0600

    some changed

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ document.getElementById('btn-submit').addEventListener('click', function () {
   const email = document.getElementById('email');
   const password = document.getElementById('pass');
 
-  if (email.value == 'baper@bank.com' && password.value == 'secret') {
+  if (email.value == 'son@baap.com' && password.value == 'i am your son') {
     window.location.href = 'bank.html'
   }
   else {
